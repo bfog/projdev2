c776888f0a09cbdcf2956a87dcd2dc97d98f726a
KFost <30683092+KFost@users.noreply.github.com>
2018-05-03 17:27:34 +1000

    Updated view js

--- script/view.js
+++ script/view.js
@@ -2,13 +2,17 @@ var app = angular.module('myApp',[]);
 app.controller('myCtrl', function($scope, $http){
 	$scope.productArray = [ {"id":"Null","name":"Skin Cream","category":"Health Care","stock":"100","price":"9.95"},{"id":"Null","name":"Fungal Toe Spray","category":"Health Care","stock":"25","price":"34.95"},{"id":"Null","name":"Toilet Paper","category":"Accessories","stock":"15","price":"9.95"},{"id":"Null","name":"Protein Powder","category":"Accessories","stock":"20","price":"49.95"},{"id":"Null","name":"Swiss Multivitiamin","category":"Medicine","stock":"200","price":"39.95"}];
 
+	$scope.currentEdit = null;
 	$scope.list = [];
-	$scope.editData= function(array, value){
+	$scope.editData= function(array){
 		array.edit = true;
-		$scope.list.push(value);
+		if($scope.currentEdit != null){
+			$scope.cancelEdit($scope.currentEdit);
+		}
+		$scope.currentEdit = array;
 	}
-	$scope.finalise = function(array, value){
+
+	$scope.cancelEdit= function(array){
 		array.edit = false;
-		$scope.list.splice(value,1);
 	}
 });
